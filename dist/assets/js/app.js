f23f1960c45eb54d5d0dc58865c21fa20656ce89
evgsbel <evgsbel@gmail.com>
2023-03-17 20:30:54 +0400

    start adaptive

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -1,6 +1,10 @@
 "use strict";
 
-function _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it["return"] != null) it["return"](); } finally { if (didErr) throw err; } } }; }
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+function _iterableToArrayLimit(arr, i) { var _i = null == arr ? null : "undefined" != typeof Symbol && arr[Symbol.iterator] || arr["@@iterator"]; if (null != _i) { var _s, _e, _x, _r, _arr = [], _n = !0, _d = !1; try { if (_x = (_i = _i.call(arr)).next, 0 === i) { if (Object(_i) !== _i) return; _n = !1; } else for (; !(_n = (_s = _x.call(_i)).done) && (_arr.push(_s.value), _arr.length !== i); _n = !0); } catch (err) { _d = !0, _e = err; } finally { try { if (!_n && null != _i["return"] && (_r = _i["return"](), Object(_r) !== _r)) return; } finally { if (_d) throw _e; } } return _arr; } }
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+function _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e2) { throw _e2; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e3) { didErr = true; err = _e3; }, f: function f() { try { if (!normalCompletion && it["return"] != null) it["return"](); } finally { if (didErr) throw err; } } }; }
 function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
 function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i]; return arr2; }
 //masked inputs
@@ -131,29 +135,20 @@ $(function () {
     closeMenu();
   });
 });
-function setVideoCenter() {
-  var $box = $('.video-box');
-  var height = $box.height();
-  var width = $box.width();
-  var new_height = width / 1.78;
-  if (new_height > height) {
-    $box.find('iframe').css({
-      width: width,
-      height: new_height,
-      top: '-' + (new_height / 2 - height / 2) + 'px',
-      left: '0'
-    });
-  } else {
-    var new_width = height * 1.78;
-    $box.find('iframe').css({
-      width: new_width,
-      height: height,
-      top: '0',
-      left: '-' + (new_width / 2 - width / 2) + 'px'
-    });
+
+// Play Video
+var $video = document.querySelector('.video-block__video');
+var observer = new window.IntersectionObserver(function (_ref) {
+  var _ref2 = _slicedToArray(_ref, 1),
+    entry = _ref2[0];
+  if (entry.isIntersecting) {
+    $video.play();
+    return;
   }
-}
-$(function () {
-  setVideoCenter();
-  $(window).resize(setVideoCenter);
+  $video.pause();
+}, {
+  root: null,
+  threshold: 0.1 // set offset 0.1 means trigger if atleast 10% of element in viewport
 });
+
+observer.observe($video);
--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -134,30 +134,21 @@ $(() => {
   });
 });
 
-function setVideoCenter() {
-  var $box = $('.video-box');
-  var height = $box.height();
-  var width = $box.width();
-  var new_height = width / 1.78;
-  if (new_height > height) {
-    $box.find('iframe').css({
-      width: width,
-      height: new_height,
-      top: '-' + (new_height / 2 - height / 2) + 'px',
-      left: '0',
-    });
-  } else {
-    var new_width = height * 1.78;
-    $box.find('iframe').css({
-      width: new_width,
-      height: height,
-      top: '0',
-      left: '-' + (new_width / 2 - width / 2) + 'px'
-    });
-  }
-}
+// Play Video
+const $video = document.querySelector('.video-block__video')
+const observer = new window.IntersectionObserver(([entry]) => {
+  if (entry.isIntersecting) {
 
-$(function(){
-  setVideoCenter();
-  $(window).resize(setVideoCenter);
-});
+    $video.play();
+    return
+  }
+  $video.pause();
+
+}, {
+  root: null,
+  threshold: 0.1, // set offset 0.1 means trigger if atleast 10% of element in viewport
+})
+
+observer.observe($video);
+
+
